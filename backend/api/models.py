6e223bf4edf934566b5e561d758564bef67e6340
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-02-26 17:31:18 +0300

    custom user model

--- backend/api/models.py
+++ backend/api/models.py
@@ -18,7 +18,8 @@ class CustomUser(AbstractUser):
     
     Role = models.CharField(max_length=40,choices=ROLE_CHOICES,default='student')
     Gender = models.CharField(max_length=20,choices=GENDER,editable=True)
-    year_of_study = models.IntegerField(choices=YEAR_CHOICES,null=True,editable=True)
+    year_of_study = models.CharField(max_length=20,choices=YEAR_CHOICES,null=True,editable=True)
+
 
     def __str__(self):
         return self.username
