b3a45ce347cd004ee8dcd6978278ddf66973b61d
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-02-26 17:00:13 +0300

    customuser model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [( 'Student','STUDENT'), 
+                    ('Lecturer', 'LECTURER'),
+                 ('Academic_registrar','ACADEMIC REGISTRAR')]
+    
+    GENDER = [('Male','MALE'),
+              ('Female','FEMALE')]
+    
+    YEAR_CHOICES = [('YEAR 1','YEAR 1'),
+            ('YEAR_2','YEAR 2'),
+            ('YEAR_3','YEAR 3'),
+            ('YEAR_4','YEAR 4'),
+            ('YEAR_5','YEAR 5')]    
+    
+    Role = models.CharField(max_length=40,choices=ROLE_CHOICES,default='student')
+    Gender = models.CharField(max_length=20,choices=GENDER,editable=True)
+    year_of_study = models.IntegerField(choices=YEAR_CHOICES,null=True,editable=True)
+
+    def __str__(self):
+        return self.username
+
+
