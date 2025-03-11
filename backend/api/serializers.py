2756c8075ca8bad19df2da032870a4bb8dd423b6
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-03-11 12:44:29 +0300

    serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -11,4 +11,13 @@ class Departmentserializer(ModelSerializer):
         model = Department
         fields = ['department_name','description']
 
-        
+class Issueserializer(ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['student','issue_type','issue_status','course_unit','issue_description','Image','date_created','update','lecturer','registrar']
+
+class CourseUnitserializer(ModelSerializer):
+    class Meta:
+        model = CourseUnit
+        fields = ['course_unit_name','course_unit_code']
+
