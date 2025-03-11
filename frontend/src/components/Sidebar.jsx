7838b806e69d3eea49a9cc6eb3ec91f890ef55eb
Alvin11David <alvin69david@gmaiol.com>
2025-03-11 23:32:43 +0300

    Enhanced sidebar with hover effects and respective icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import './Sidebar.css';
+import dashboard from '../icons/dashboard.png';
+import issue from '../icons/issue.png';
+import person from '../icons/person.png';
+import settings from '../icons/settings.png';
+import support from '../icons/support.png';
+import logout from '../icons/logout.png';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
             <ul className='sidebar-menu'>
-                <li className='sidebar-item'>Dashboard</li>
-                <li className='sidebar-item'>Issues</li>
-                <li className='sidebar-item'>Profile</li>
-                <li className='sidebar-item'>Settings</li>
-                <li className='sidebar-item'>Help & Support</li>
-                <li className='sidebar-item'>Logout</li>
+                <li className='sidebar-item'>
+                <img src={dashboard} alt='dashboard' className='sidebar-icon'/>
+                    Dashboard</li>
+                <li className='sidebar-item'>
+                    <img src={issue} alt='issue' className='sidebar-icon'/>
+                    Issues</li>
+                <li className='sidebar-item'>
+                    <img src={person} alt='person' className='sidebar-icon'/>
+                    Profile</li>
+                <li className='sidebar-item'>
+                    <img src={settings} alt='settings' className='sidebar-icon'/>
+                    Settings</li>
+                <li className='sidebar-item'>
+                    <img src={support} alt='support' className='sidebar-icon'/>
+                    Help & Support</li>
+                <li className='sidebar-item logout'>
+                    <img src={logout} alt='logout' className='sidebar-icon'/>
+                    Logout</li>
             </ul>
         </div>
     );
