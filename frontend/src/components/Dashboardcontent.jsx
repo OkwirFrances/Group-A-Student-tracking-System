77e7e4ee0ebe687651d4425937342e4f10614c21
Alvin11David <alvin69david@gmaiol.com>
2025-03-12 23:05:25 +0300

    Added styles to the stat cards on the student dashboard

--- frontend/src/components/Dashboardcontent.jsx
+++ frontend/src/components/Dashboardcontent.jsx
@@ -10,17 +10,17 @@ const DashboardContent = () => {
         <div className='dashboard-content'>
             <h1>Dashboard</h1>
             <div className='cards-container'>
-                <div className='card'>
+                <div className='card pending'>
                     <h2>Pending Issues</h2>
                     <p className='issue-count'>{pendingIssues}</p>
                     <p>You have {pendingIssues} pending issues.</p>
                 </div>
-                <div className='card'>
+                <div className='card in-progress'>
                     <h2>In-progress Issues</h2>
                     <p className='issue-count'>{inprogressIssues}</p>
                     <p>You have {inprogressIssues} in-progress issues.</p>
                 </div>
-                <div className='card'>
+                <div className='card resolved'>
                     <h2>Resolved Issues</h2>
                     <p className='issue-count'>{resolvedIssues}</p>
                     <p>You have {resolvedIssues} resolved issues.</p>
