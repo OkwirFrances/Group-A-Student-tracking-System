d7614983f2d123375cfb6741a22cb53704fe3f9f
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-03-12 11:03:10 +0300

    program class

--- backend/api/models.py
+++ backend/api/models.py
@@ -18,6 +18,8 @@ class CustomUser(AbstractUser):
     
     Role = models.CharField(max_length=40,choices=ROLE_CHOICES,default='Student')
     Gender = models.CharField(max_length=20,choices=GENDER,editable=True)
+    image = models.ImageField(upload_to='issues/',null=True,blank=True)
+    
     year_of_study = models.CharField(max_length=20,choices=YEAR_CHOICES,null=True,editable=True)
 
 
@@ -25,18 +27,22 @@ class CustomUser(AbstractUser):
         return self.username
 
 class Department(models.Model):
-    department_name = models.CharField(max_length=100)
+    department_name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
     
     def __str__(self):
         return self.department_name
-    
+
 class CourseUnit(models.Model):
     course_unit_name = models.CharField(max_length=100)
     course_unit_code = models.CharField(max_length=100)
 
     def __str__(self):
         return self.course_unit_name
+
+class Program(models.Model):
+    program_name = models.CharField(max_length=100)
+    course_units = models.ManyToManyField(CourseUnit, related_name="course_units")
 
 
 class Issue(models.Model):
@@ -48,10 +54,15 @@ class Issue(models.Model):
                      ('appeal','APPEAL'),
                      ('correction','CORRECTION')]
     
+    SEMESTER_CHOICES = [('Semester 1','SEMESTER 1'),
+                        ('Semester 2','SEMESTER 2')]
+    
     student = models.ForeignKey(CustomUser,on_delete=models.SET_NULL,null=True,related_name='issues', limit_choices_to={'Role':'Student'})
+    semester = models.CharField(max_length=30, null = False,default='Semester 1')
     issue_type = models.CharField(max_length=50,choices=ISSUE_CHOICES)
     issue_status = models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
-    course_unit = models.ForeignKey('CourseUnit',on_delete=models.SET_NULL,null=True)
+    program = models.ForeignKey(Program, on_delete=models.CASCADE,related_name='programs',null = True,blank=True)
+    course_unit = models.ForeignKey(CourseUnit,on_delete=models.CASCADE,null=True)
     issue_description = models.TextField()
     Image = models.ImageField(upload_to='images/',null=True,blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
