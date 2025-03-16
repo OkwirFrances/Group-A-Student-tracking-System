43a3c0cdddd03bb7f47a691072af3ed1e566332d
Alvin11David <alvin69david@gmaiol.com>
2025-03-16 23:16:16 +0300

    Added changes to the issue details component

--- frontend/src/components/issuedetails.jsx
+++ frontend/src/components/issuedetails.jsx
@@ -3,3 +3,18 @@ import { useParams } from 'react-router-dom';
 import { IssuesContext } from '../context/IssueContext';
 import './issuedetails.css';
 
+const IssueDetails = () => {
+    const { id } = useParams();
+    const { issues } = useContext(IssuesContext);
+    const issue = issues.find(issue => issue.id === id);
+
+    if (!issue) {
+        return <div>Issue not found</div>;
+    }
+
+    return (
+        <div className='issue-detail-container'>
+            
+        </div>
+    )
+};
