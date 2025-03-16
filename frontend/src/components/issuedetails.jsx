de2d1e92b4992c9c67e7e0cfb15628f33e2d3872
Alvin11David <alvin69david@gmaiol.com>
2025-03-16 23:23:43 +0300

    Completed the issue details jsx

--- frontend/src/components/issuedetails.jsx
+++ frontend/src/components/issuedetails.jsx
@@ -14,7 +14,26 @@ const IssueDetails = () => {
 
     return (
         <div className='issue-detail-container'>
-            
+            <h1>Issue Details</h1>
+            <div className='issue-detail'>
+                <p><strong>Title:</strong>{issue.title}</p>
+                <p><strong>Description:</strong>{issue.description}</p>
+                <p><strong>Category:</strong>{issue.category}</p>
+                <p><strong>Registrar</strong>{issue.registrar}</p>
+                <p><strong>Lecturer:</strong>{issue.lecturer}</p>
+                <p><strong>Course Name:</strong>{issue.coursename}</p>
+                <p><strong>Course Code:</strong>{issue.coursecode}</p>
+                <p><strong>Status:</strong>{issue.status}</p>
+                <p><strong>Date:</strong>{issue.date}</p>
+                {issue.attachment && (
+                    <div>
+                        <strong>Attachment:</strong>
+                        <img src={URL.createObjectURL(issue.attachment)} alt='attachment' className='issue-attachment' />
+                    </div>
+                )}
+            </div>
         </div>
-    )
+    );
 };
+
+export default IssueDetails;
