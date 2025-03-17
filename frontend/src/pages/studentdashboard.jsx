d356ddd174e9dce8090b2e06d7f5c32aec2ec72d
Kalyangotimothy <148437140+Kalyangotimothy@users.noreply.github.com>
2025-03-17 17:46:16 +0300

    'update'

--- frontend/src/pages/studentdashboard.jsx
+++ frontend/src/pages/studentdashboard.jsx
@@ -4,18 +4,17 @@ import './studentdashboard.css';
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
-
-
 const StudentDashboard = () => {
     return (
-            <div className="dashboard-container">
-                <Navbar />
+        <div className="dashboard-container">
+            <Navbar />
+            <div className="main-layout">
                 <Sidebar />
                 <div className="dashboard-content">
                     <Outlet/>
                 </div>
             </div>
-       
+        </div>
     );
 };
 
