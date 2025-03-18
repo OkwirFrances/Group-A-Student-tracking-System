0e603bd4f48feea1a1d6d8013c0b88002d60afed
Lisacode100 <eageno075@gmail.com>
2025-03-18 21:37:41 +0300

    urls user roles update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 from .views import *
+from .views import get_course_units , get_users_by_role
 
 router = DefaultRouter()
 router.register(r'users',UserView)
@@ -16,7 +17,8 @@ router.register(r'programs',ProgramView)
 
 urlpatterns = [
     path('',include(router.urls)),
-    
+    path('api/users/', get_users_by_role, name = 'get_users_by_role'),
+    path('api/users/', get_course_units, name = 'get_course_units'),
     path('signup/',SignUpView.as_view(),name = 'signup'),
     path('access_token/',TokenObtainPairView.as_view(),name = 'access_token'),
     path('refresh_token/',TokenRefreshView.as_view(),name = 'refresh_token')
