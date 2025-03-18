5035abab83ae4a9880f22d7c4c53c0679529924c
Lisacode100 <eageno075@gmail.com>
2025-03-18 21:08:41 +0300

    customized admin.py to filter dropdowns

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -7,5 +7,20 @@ admin.site.register(Issue)
 admin.site.register(CourseUnit)
 admin.site.register(Program)
 
-
+class IssueAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        
+        if db_field.name == 'student':
+            kwargs['queryset'] = CustomUser.objects.filter(Role = 'Student')
+            
+        elif db_field.name == 'lecturer':
+            kwargs['queryset'] = CustomUser.objects.filter(Role = 'Lecturer')
+            
+        elif db_field == 'registrar':
+            kwargs['queryset'] = CustomUser.objects.filter(Role = 'Academic_Registrar')
+            
+            return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    
+                
+        admin.site.register(Issue, IssueAdmin)
 
