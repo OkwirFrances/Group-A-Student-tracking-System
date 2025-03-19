135c8af35ccd1a594ca78354abedbf05bd0bb2ea
Alvin11David <alvin69david@gmaiol.com>
2025-03-19 07:30:12 +0300

    Added css styles to the phone contacts

--- frontend/src/pages/helpsupport.jsx
+++ frontend/src/pages/helpsupport.jsx
@@ -14,7 +14,7 @@ const HelpSupport = () => {
                     <h2>Kindly reach us via Email or Phone<br /> for any help </h2>
                     <p>Email</p>
                     <h3>Send us an email on; <br />alvin69david@gmail.com</h3>
-                    <p>Phone Contacts</p>
+                    <p className='phone-contacts'>Phone Contacts</p>
                 </div>
             </div>
         </div>
