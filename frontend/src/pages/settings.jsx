ec803abbf58c11ee575cfde6e3c473fb90c1c7c9
Alvin11David <alvin69david@gmaiol.com>
2025-03-19 15:14:31 +0300

    Added the settings page

---
+++ frontend/src/pages/settings.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import './settings.css';
+
+const Settings = () => {
+    return (
+        <div className='settings-container'>
+            <Sidebar />
+            <div className='settings-content'>
+                <Navbar />
+                <h1>Settings</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Settings;
