0b06e2333447e3f3e6a1ec650e3dd646c7a1fd69
Alvin11David <alvin69david@gmaiol.com>
2025-03-21 17:44:22 +0300

    Added text below the settings icon

--- frontend/src/pages/settings.jsx
+++ frontend/src/pages/settings.jsx
@@ -14,6 +14,7 @@ const Settings = () => {
                 <h1>Settings</h1>
                 <div className='settings-box'>
                     <img src={settings} alt='settings' className='settings' />
+                    <h2>Tap one of the tabs to appear here</h2>
                     <div className='settings-tab'>
                         <img src={logo} alt='muk-logo' className='muklogo' />
                     </div>
