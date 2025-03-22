7b97640f96907f89b988a429427e951beffdcd7d
okwir francis <okwirfrancis2010@gmail.com>
2025-03-22 18:23:47 +0300

    Removed create_super_user from usermanager

--- backend/api/models.py
+++ backend/api/models.py
@@ -18,15 +18,6 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        """
-        Create and return a superuser with an email and password.
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self.create_user(email, password, **extra_fields)
-
 class User(AbstractUser):
     username = None  # Removing the username field
     email = models.EmailField(unique=True)
