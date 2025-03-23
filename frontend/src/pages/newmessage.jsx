9fac7fd53523ef2948b16af7c8bf5bfab327c694
Alvin11David <alvin69david@gmaiol.com>
2025-03-23 17:41:08 +0300

    Added a list of users to select

--- frontend/src/pages/newmessage.jsx
+++ frontend/src/pages/newmessage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import './newmessage.css';
@@ -6,12 +6,26 @@ import backarrow from '../assets/backarrow.png';
 import search from '../assets/search.png';
 import emptybox from '../assets/emptybox.png';
 import newmessage from '../assets/newmessage.png';
+import userimage from '../assets/userimage.png';
+
+const users = [
+    { id: 1 , name:'Dr. Alvin David' , profilePic: userimage},
+    { id: 2, name: 'Dr. Jane Ritah', profilePic: userimage},
+    { id: 3, name: 'Dr. Daniella M', profilePic: userimage},
+    { id: 4, name: 'Dr. Wakholi J', profilePic: userimage},
+    { id: 5, name: 'Dr. Aloi Lugra', profilePic: userimage},
+];
 
 const NewMessage = () => {
     const navigate = useNavigate();
+    const [showUserList, setShowUserList] = useState(false);
 
     const handleBackMessageClick = () => {
         navigate('/messages');
+    };
+
+    const handleNewMessageClick = () => {
+        setShowUserList(true);
     };
 
     return (
@@ -34,9 +48,26 @@ const NewMessage = () => {
                     />
                     <img src={search} alt='search' className='search' />
                 </div>
-                <img src={emptybox} alt='emptybox' className='newmessage-emptybox' />
-                <p>Click the button below to start a new chat.</p>
-                <img src={newmessage} alt='newmessage' className='newmessage' />
+                {!showUserList ? (
+                    <>
+                        <img src={emptybox} alt='emptybox' className='newmessage-emptybox' />
+                        <p>Click the button below to start a new chat.</p>
+                        <img 
+                        src={newmessage} 
+                        alt='newmessage' 
+                        className='newmessage'
+                        onClick={handleNewMessageClick} />
+                    </>
+                ) : (
+                    <div className='user-list'>
+                        {users.map(user => (
+                            <div key={user.id} className='user-item'>
+                                <img src={user.profilePic} alt={user.name} className="user-profile-pic" />
+                                <span className='user-name'>{user.name}</span>
+                            </div>   
+                        ))}
+                    </div>
+                )}
             </div>
             <div className='newmessage-right'>
                 <nav>Chat Area</nav>
