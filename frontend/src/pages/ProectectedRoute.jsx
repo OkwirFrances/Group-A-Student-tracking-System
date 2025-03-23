e9af596511f13109089d8e6809494e1b5a38f707
okwir francis <okwirfrancis2010@gmail.com>
2025-03-23 16:06:51 +0300

    Updated protected routes

--- frontend/src/pages/ProectectedRoute.jsx
+++ frontend/src/pages/ProectectedRoute.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ requiredRole }) => {
     const token = localStorage.getItem('authToken');
+    const userRole = localStorage.getItem('userRole');
 
     // If the user is not authenticated, redirect to the sign-in page
     if (!token) {
         return <Navigate to="/signin" />;
     }
 
-    // If authenticated, render the child routes
+    // If the user's role does not match the required role, redirect to the sign-in page
+    if (userRole !== requiredRole) {
+        return <Navigate to="/signin" />;
+    }
+
+    // If authenticated and the role matches, render the child routes
     return <Outlet />;
 };
 
