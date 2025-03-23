4dd4129483e6cc9e4322fd14bbf5eb3b40eb72b5
Alvin11David <alvin69david@gmaiol.com>
2025-03-23 14:57:15 +0300

    Added a div section with a search bar and emptybox

--- frontend/src/pages/newmessage.jsx
+++ frontend/src/pages/newmessage.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import './newmessage.css';
 import backarrow from '../assets/backarrow.png';
+import search from '../assets/search.png';
+import emptybox from '../assets/emptybox.png';
 
 const NewMessage = () => {
+    const navigate = useNavigate();
+
+    const handleBackMessageClick = () => {
+        navigate('/messages');
+    };
+
     return (
         <div className='newmessage-container'>
             <Navbar />
             <div className='newmessage-content'>
-                <img src={backarrow} alt='backarrow' className='back-arrow' />
+                <img 
+                src={backarrow} 
+                alt='backarrow' 
+                className='back-arrow'
+                onClick={handleBackMessageClick} />
                 <h1>New Message</h1>
+            </div>
+            <div className='newmessage-left'>
+                <div className='searchcontainer'>
+                    <input 
+                    type='text'
+                    className='searchinput'
+                    placeholder='Search for anything...'
+                    />
+                    <img src={search} alt='search' className='search' />
+                </div>
+                <img src={emptybox} alt='emptybox' className='newmessage-emptybox' />
             </div>
         </div>
     );
