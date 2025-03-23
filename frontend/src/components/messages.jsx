f305fb6ba4e1944788140a0893a89b684e95d864
Alvin11David <alvin69david@gmaiol.com>
2025-03-23 14:12:54 +0300

    Added some css styles to the back arrow

--- frontend/src/components/messages.jsx
+++ frontend/src/components/messages.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './messages.css';
 import Navbar from './Navbar';
 import backarrow from '../assets/backarrow.png';
 import emptybox from '../assets/emptybox.png';
+
 const Messages = () => {
+
+    const navigate = useNavigate();
+
+    const handleBackClick = () => {
+        navigate('/app/dashboard');
+    };
+
     return (
         <div className='messages-container'>
             <Navbar />
             <div className='messages-content'>
                 <h1>Messages</h1>
-                <img src={backarrow} alt='backarrow' className='backarrow'/>
+                <img 
+                src={backarrow} 
+                alt='backarrow' 
+                className='backarrow'
+                onClick={handleBackClick}/>
                 <div className='content'>
                     <button 
                     className='new-message'
-                    >New Message</button>
+                    >New Message
+                    </button>
                     <img src={emptybox} alt='emptybox' className='empty-box'/>
                     <h2>There are no messages here.</h2>
                     <p>You will get notified when you get a message which will appear here.<br/>You can also start a new message by clicking <b>New Message</b>.</p>
