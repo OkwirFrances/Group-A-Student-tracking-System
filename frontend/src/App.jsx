503bbcabf167bec4706e3095d834aecfc62707c3
okwir francis <okwirfrancis2010@gmail.com>
2025-03-23 16:05:24 +0300

    Added Dashboards routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,10 @@ import SignIn from './pages/signin';
 import IssueForm from './pages/issueform';
 import Congratulations from './pages/congratulations';
 import StudentDashboard from './pages/studentdashboard';
+import LecturerDashboard from './pages/LecturerDashboard';
+import RegistrarDashboard from './pages/RegistrarDashboard';
 import './App.css';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Otp from './pages/otp';
 import DashboardContent from './components/Dashboardcontent';
 import IssueDetails from './components/issuedetails';
@@ -15,29 +17,67 @@ import HelpSupport from './pages/helpsupport';
 import Settings from './pages/settings';
 import { IssuesProvider } from './context/IssueContext';
 import ErrorBoundary from './pages/ErrorBoundary';
-
-
+import ProtectedRoute from './components/ProtectedRoute'; // Import the ProtectedRoute component
 
 const App = () => {
-
   return (
-    <IssuesProvider >
+    <IssuesProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" index element={<Navigate to="landing"/>}/>
-          <Route path="landing" element={<ErrorBoundary><LandingPage/></ErrorBoundary>}/>
-          <Route path="signup" element={<ErrorBoundary><SignUp/></ErrorBoundary>}/>
-          <Route path="signin" element={<SignIn/>}/>
-          <Route path="otp" element={<Otp/>}/>
-          <Route path="/congratulations" element={<Congratulations/>}/>
-          <Route path="app" element={<StudentDashboard/>}>
-            <Route path="dashboard" element={ <DashboardContent />}/>
-            <Route path='issueform' element={<IssueForm />}/>
-            <Route path="issue/:id" element={<IssueDetails />}/>
-            <Route path='support' element={<HelpSupport />}/>
+          {/* Public Routes */}
+          <Route path="/" index element={<Navigate to="landing" />} />
+          <Route path="landing" element={<ErrorBoundary><LandingPage /></ErrorBoundary>} />
+          <Route path="signup" element={<ErrorBoundary><SignUp /></ErrorBoundary>} />
+          <Route path="signin" element={<SignIn />} />
+          <Route path="otp" element={<Otp />} />
+          <Route path="/congratulations" element={<Congratulations />} />
+
+          {/* Protected Routes */}
+          <Route
+            path="student"
+            element={
+              <ProtectedRoute requiredRole="student">
+                <StudentDashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="dashboard" element={<DashboardContent />} />
+            <Route path="issueform" element={<IssueForm />} />
+            <Route path="issue/:id" element={<IssueDetails />} />
+            <Route path="support" element={<HelpSupport />} />
             <Route path="settings" element={<Settings />} />
           </Route>
-          <Route path='notifications' element={<NotificationScreen />}/>
+
+          <Route
+            path="lecturer"
+            element={
+              <ProtectedRoute requiredRole="lecturer">
+                <LecturerDashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="dashboard" element={<DashboardContent />} />
+            <Route path="issue/:id" element={<IssueDetails />} />
+            <Route path="support" element={<HelpSupport />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+
+          <Route
+            path="registrar"
+            element={
+              <ProtectedRoute requiredRole="registrar">
+                <RegistrarDashboard />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="dashboard" element={<DashboardContent />} />
+            <Route path="issue/:id" element={<IssueDetails />} />
+            <Route path="support" element={<HelpSupport />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+
+          {/* Notifications Route */}
+          <Route path="notifications" element={<NotificationScreen />} />
         </Routes>
       </BrowserRouter>
     </IssuesProvider>
