df07cbe26e2610e84f94b2f350e336deb5c64694
okwir francis <okwirfrancis2010@gmail.com>
2025-03-23 12:24:06 +0300

    modified settings for otp email sending to my mail

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -168,3 +168,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = 'okwirfrancis2010@gmail.com'  
+EMAIL_HOST_PASSWORD = 'your-email-password'  # Your email password (or app-specific password)
+
+
