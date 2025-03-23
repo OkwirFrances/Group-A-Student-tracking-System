37e0eb3ac8cbadf0d3415aea6779092ca5b0b05a
okwir francis <okwirfrancis2010@gmail.com>
2025-03-23 12:28:48 +0300

    added django-otp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'django_otp',
+    'django_otp.plugins.otp_totp',
+    'django.contrib.auth',
     
 ]
 
