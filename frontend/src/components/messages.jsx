601e26caf04f4f7bfc42a1473c275fe6a90e7f07
Alvin11David <alvin69david@gmaiol.com>
2025-03-23 13:40:27 +0300

    Added the New Message button

--- frontend/src/components/messages.jsx
+++ frontend/src/components/messages.jsx
@@ -11,6 +11,9 @@ const Messages = () => {
                 <h1>Messages</h1>
                 <img src={backarrow} alt='backarrow' className='backarrow'/>
                 <div className='content'>
+                    <button 
+                    className='new-message'
+                    >New Message</button>
                     <img src={emptybox} alt='emptybox' className='empty-box'/>
                     <h2>There are no messages here.</h2>
                     <p>You will get notified when you get a message which will appear here.<br/>You can also start a new message by clicking <b>New Message</b>.</p>
