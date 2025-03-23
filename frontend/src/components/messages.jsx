a34b85e0faf29593d8340ddae6f7bf88dd1f915e
Alvin11David <alvin69david@gmaiol.com>
2025-03-23 13:34:06 +0300

    Added an emptybox icon

--- frontend/src/components/messages.jsx
+++ frontend/src/components/messages.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './messages.css';
 import Navbar from './Navbar';
 import backarrow from '../assets/backarrow.png';
-
+import emptybox from '../assets/emptybox.png';
 const Messages = () => {
     return (
         <div className='messages-container'>
@@ -10,7 +10,8 @@ const Messages = () => {
             <div className='messages-content'>
                 <h1>Messages</h1>
                 <img src={backarrow} alt='backarrow' className='backarrow'/>
-                <div>
+                <div className='content'>
+                    <img src={emptybox} alt='emptybox' className='empty-box'/>
                     <h2>There are no messages here.</h2>
                     <p>You will get notified when you get a message which will appear here.<br/>You can also start a new message by clicking <b>New Message</b>.</p>
                 </div>
