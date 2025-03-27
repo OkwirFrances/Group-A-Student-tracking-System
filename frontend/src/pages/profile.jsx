332b47120a328a0b2fa409c2eae2ec6d7c523cd6
martinanamuli <martinanamuli@icloud.com>
2025-03-27 15:54:54 +0300

    I have added and styled profile and full name label

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import './profile.css';
 
 const Profile = () => {
+    const [user] = useState({
+        fullName: 'Alvin David',
+        profilePic: null,  
+    });
+
+    const getInitials =(name) => {
+        return name.charAt(0).toUpperCase();
+    };
+
     return (
         <div className='profile-container'>
             <Sidebar />
             <div className='profile-content'>
                 <Navbar />
                 <h1>Profile</h1>
-                <div className='profile'></div>
-                <div className='profile-section'></div>
+                <div className='profile'>
+                    <div className="profile-picture-container">
+                        {user.profilePic ? (
+                            <img src={user.profilePic} alt="Profile" className="profile-picture" />
+                        ) : (
+                                <div className="profile-initials">
+                                    {getInitials(user.fullName)}
+                                </div>
+                        )}
+                        <div className="profile-details">
+                            <label className='fullname'>Full Name:</label>
+                            <h2>{user.fullName}</h2>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </div>
     );
