4dc37782c3fee76ed299af6d6dd54f781cc2deed
Alvin11David <alvin69david@gmaiol.com>
2025-03-30 00:07:35 +0300

    Adde the registrar dashboard

--- frontend/src/pages/registrardashboard.jsx
+++ frontend/src/pages/registrardashboard.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import Navbar from './Navbar';
-import Sidebar from './Sidebar';
-import './registrardashboard.css';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import './studentdashboard.css';
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+
 
 const RegistrarDashboard = () => {
+    const userRole = localStorage.getItem('userRole');
     return (
-        <div>
-            <Sidebar />
-            <div>
+            <div className="dashboard-container">
+                <h1>Registrar Dashboard</h1>
+                <p>Welcome {userRole}</p>
                 <Navbar />
-                <h1>Lecturer Dashboard</h1>
+                <Sidebar />
+                <div className="dashboard-content">
+                    <Outlet/>
+                </div>
             </div>
-        </div>
+       
     );
 };
 
