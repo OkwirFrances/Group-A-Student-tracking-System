539a5cebc71409997d4e7bef7d533612b446612b
okwir francis <okwirfrancis2010@gmail.com>
2025-03-29 15:51:54 +0300

    restored

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
-from .views import signup, verify_otp, login, resend_otp, DepartmentView, CourseView, IssueView, assign_issue, resolve_issue, UserInfoView, UserEditView
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from .views import *
 
+router = DefaultRouter()
+router.register(r'users',UserView)
+
+router.register(r'departments',DepartmentView)
+
+router.register(r'issues',IssueView)
+
+router.register(r'course_units',CourseUnitView)
+
+router.register(r'programs',ProgramView)
 
 urlpatterns = [
     path('',include(router.urls)),
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,8 @@
-# views.py
-
-import random
-from django.http import JsonResponse
-from django.core.mail import send_mail
-from django.contrib.auth import get_user_model
-from rest_framework import status, generics
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *   
+from .serializers import *
+from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny,IsAuthenticated
@@ -21,27 +19,9 @@ class UserView(viewsets.ModelViewSet):
 class DepartmentView(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
-    permission_classes = [IsAuthenticated, IsRegistrar]  # Only registrars can create departments
 
-    def get_queryset(self):
-        return Department.objects.all()
-
-    def perform_create(self, serializer):
-        serializer.save()
-
-# Course View (Only accessible by registrars)
-class CourseView(generics.ListCreateAPIView):
-    serializer_class = CourseSerializer
-    permission_classes = [IsAuthenticated, IsRegistrar]  # Only registrars can create courses
-
-    def get_queryset(self):
-        return Course.objects.all()
-
-    def perform_create(self, serializer):
-        serializer.save()
-
-# Issue View (Only accessible by students for their own issues)
-class IssueView(generics.ListCreateAPIView, generics.RetrieveUpdateDestroyAPIView):
+class IssueView(viewsets.ModelViewSet):
+    queryset = Issue.objects.all()
     serializer_class = IssueSerializer
 
 class CourseUnitView(viewsets.ModelViewSet):
