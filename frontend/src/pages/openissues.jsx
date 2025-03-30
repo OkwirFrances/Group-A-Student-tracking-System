08253817f9d317060dded7c1adb0cd7b4c78444b
Alvin11David <alvin69david@gmaiol.com>
2025-03-30 23:48:15 +0300

    Added thefilter and issue  navigation

--- frontend/src/pages/openissues.jsx
+++ frontend/src/pages/openissues.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
+import search from '../assets/search.png';
+import filter from '../assets/filter.png';
+import emptybox from '../assets/emptybox.png';
 import './openissues.css';
 
 const OpenIssues = () => {
@@ -15,7 +18,36 @@ const OpenIssues = () => {
                     <button className='nav-button'>In-Progress</button>
                     <button className='nav-button'>Resolved</button>
                 </div>
-                <div className='issue'></div>
+                <div className='issue'>
+                    <p>Issues</p>
+                    <div className='myissuescontainer'>
+                        <input 
+                            type='text' 
+                            placeholder='Search for anything...' 
+                            className='myissuesinput' />
+                        <img src={search} alt='search' className='myissuesicon' />
+                    </div>
+                </div>
+                <div className='filter-issues-container'>
+                    <select className='filter-issues' >
+                        <option value='all'>All</option>
+                        <option value='pending'>Pending</option>
+                        <option value='in-progress'>In-progress</option>
+                        <option value='resolved'>Resolved</option>
+                        <img src={filter} alt='filter' className='issuefiltersicon' /> 
+                    </select>
+                </div>
+                <div className='table'>
+                    <div className='header'>
+                        <div className='item'>Issue</div>
+                        <div className='item'>Status</div>
+                        <div className='item'>Category</div>
+                        <div className='item'>Date</div>
+                    </div>
+                    <div className='body'>
+                        <img src={emptybox} alt="emptybox" className="emptybox" />
+                    </div>
+                </div>
             </div>
         </div>
     );
