2e8de911e2d0bac139cd30eeff18b6b2f04f0d39
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-03-31 11:13:50 +0300

    Modified permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,6 +2,7 @@
 
 from rest_framework import permissions
 
+
 class IsRegistrar(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.role == 'registrar'
