6ff37df8a59c3c95fdf3a4be42b9e2ac27fc830d
Lisacode100 <eageno075@gmail.com>
2025-04-02 07:36:57 +0300

    customuser admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -6,6 +6,14 @@ from django.contrib.auth.admin import UserAdmin
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ('email','fullname','role', 'is_verified', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_verified', 'is_staff', 'is_active')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('fullname', 'phone_number', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Verification', {'fields': ('otp', 'is_verified')}),
+    )
     
 admin.site.register(CustomUser)
 admin.site.register(Department)
