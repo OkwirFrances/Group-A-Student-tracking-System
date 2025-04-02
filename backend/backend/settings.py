f72bbfdf38f43e27864bb5fba4ae4a61c65963bb
Lisacode100 <eageno075@gmail.com>
2025-04-02 07:23:30 +0300

    email settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -165,3 +165,10 @@ CSRF_COOKIE_HTTPONLY = False
 CSRF_USE_SESSIONS = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True  
+EMAIL_HOST_USER = 'okwirfrancis2010@gmail.com'  
+EMAIL_HOST_PASSWORD = 'pptl jkld aodn cmrt'  
