50be31a9a27bd00817c8077439640671a6240c5e
Lisacode100 <eageno075@gmail.com>
2025-04-02 07:36:44 +0300

    customuser admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import CustomUser,Department, Issue
+from .models import CustomUser,Department, Issue, Course, Lecturer, Student, Registrar
+from django.contrib.auth.admin import UserAdmin
 
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email','fullname','role', 'is_verified', 'is_staff', 'is_active')
+    
 admin.site.register(CustomUser)
 admin.site.register(Department)
 admin.site.register(Issue)
