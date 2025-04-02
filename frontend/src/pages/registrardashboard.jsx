2f6a2bde3588f415a71f0c5404d0896d5c072206
Alvin11David <alvin69david@gmaiol.com>
2025-04-02 13:55:22 +0300

    Modified registrardashboard

--- frontend/src/pages/registrardashboard.jsx
+++ frontend/src/pages/registrardashboard.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import './registrardashboard.css';
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
+import './studentdashboard.css';
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 
 
 
@@ -10,6 +10,8 @@ const RegistrarDashboard = () => {
     const userRole = localStorage.getItem('userRole');
     return (
             <div className="dashboard-container">
+                <h1>Registrar Dashboard</h1>
+                <p>Welcome {userRole}</p>
                 <Navbar />
                 <Sidebar />
                 <div className="dashboard-content">
