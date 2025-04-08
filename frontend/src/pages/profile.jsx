df8737d6f0894a8edf8b4f925635da137603d3f0
martinanamuli <martinanamuli@icloud.com>
2025-04-08 16:33:45 +0300

    I have added the phone number element to the student profile

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -7,7 +7,13 @@ import edit from '../assets/edit.png';
 
 const Profile = () => {
     const [user] = useState({
-        fullName: 'Alvin David',
+        fullName: '',
+        phoneNumber: '',
+        gender: '',
+        registrationNumber: '',
+        studentNumber: '',
+        course: '',
+        semester: '',
         profilePic: null,  
     });
 
@@ -43,7 +49,28 @@ const Profile = () => {
                 </div>
                 <div className='personal-information'>
                     <h1>Personal Information </h1>
+                    <label className='phone'>Phone Number:</label>
+                    <h2 className='phone-number'>{user.phone}</h2>
+                    <label className='gender'>Gender:</label>
+                    <h2>{user.gender}</h2>
+                    <button className='personal-information-editbutton'>Edit
+                        <img src={edit} alt='edit' className='edit' />
+                    </button>
                 </div>
+                <div className='academic-information'>
+                    <h1>Academic Information</h1>
+                    <label className='registration'>Registration Number:</label>
+                    <h2 className='registration-number'>{user.registrationNumber}</h2>
+                    <label className='student'>Student Number:</label>
+                    <h2 className='student-number'>{user.studentNumber}</h2>
+                    <label className='course'>Course:</label>
+                    <h2 className='course-name'>{user.course}</h2>
+                    <label className='semester'>Semester:</label>
+                    <h2 className='semester-name'>{user.semester}</h2>
+                    <button className='academic-information-editbutton'>Edit
+                        <img src={edit} alt='edit' className='edit' />
+                    </button>
+                </div>    
 
             </div>
         </div>
