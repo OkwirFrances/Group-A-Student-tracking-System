d8446f6644670d434a4f90475657963bf227eab2
Alvin11David <alvin69david@gmail.com>
2025-04-11 15:06:46 +0300

    Resolved registrar dashboard

---
+++ frontend/src/pages/registrardashboard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import './studentdashboard.css';
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+
+
+
+const RegistrarDashboard = () => {
+    const userRole = localStorage.getItem('userRole');
+    return (
+            <div className="dashboard-container">
+                <h1>Registrar Dashboard</h1>
+                <p>Welcome {userRole}</p>
+                <Navbar />
+                <Sidebar />
+                <div className="dashboard-content">
+                    <Outlet/>
+                </div>
+            </div>
+       
+    );
+};
+
+export default RegistrarDashboard;
