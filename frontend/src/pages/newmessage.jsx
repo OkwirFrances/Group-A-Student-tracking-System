477079667233ecbbd8f4349f26d4978251899955
Alvin11David <alvin69david@gmail.com>
2025-04-12 17:35:38 +0300

    Modified newmessage

--- frontend/src/pages/newmessage.jsx
+++ frontend/src/pages/newmessage.jsx
@@ -92,10 +92,10 @@ const NewMessage = () => {
                             className='newmessage-emptybox' />
                         <p>Click the button below to start a new chat.</p>
                         <img 
-                        src={newmessage} 
-                        alt='newmessage' 
-                        className='newmessage'
-                        onClick={handleNewMessageClick} />
+                            src={newmessage} 
+                            alt='newmessage' 
+                            className='newmessage'
+                            onClick={handleNewMessageClick} />
                     </>
                 ) : (
                     <div className='user-list'>
@@ -160,7 +160,7 @@ const NewMessage = () => {
                 </div>
             ) : (
                 <div>
-                    <nav>Chat Area</nav>
+                    <nav className='chatarea'>Chat Area</nav>
                     <div className='chat-box'>
                         <h2 className='chatmessage'>Select a chat for it to appear here by clicking on it.</h2>
                     </div>
