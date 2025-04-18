286874da8eedc7422991c25f359fdcded500b367
Lisacode100 <eageno075@gmail.com>
2025-04-19 01:20:26 +0300

    modified class AuthTest to class TestAuth

--- backend/api/tests/test_auth.py
+++ backend/api/tests/test_auth.py
@@ -1,12 +1,13 @@
 from rest_framework.test import APITestCase
-#from django.urls import reverse
+from django.urls import reverse
 from django.core.cache import cache
 from django.contrib.auth import get_user_model
 from rest_framework import status
 
 User = get_user_model()
 
-class AuthTests(APITestCase):
+class TestAuth(APITestCase):
+
     def setUp(self):
         self.signup_url = '/signup/'
         self.verify_url = '/verify-otp/'
