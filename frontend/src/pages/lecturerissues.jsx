0f136e45d28a313720fbad0c7337892c032bc5f0
Alvin11David <alvin69david@gmail.com>
2025-04-18 23:16:20 +0300

    Made the lecturer issues const

--- frontend/src/pages/lecturerissues.jsx
+++ frontend/src/pages/lecturerissues.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
 import './lecturerissues.css';
 import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+import search from '../assets/search.png';
+import emptybox from '../assets/emptybox.png';
+
+
+const LecturerIssues = () => {
+    
+};
