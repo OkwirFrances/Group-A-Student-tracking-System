ba7cc1b39c74f070913a953a4aca55ee40556c38
Alvin11David <alvin69david@gmail.com>
2025-04-23 22:18:20 +0300

    modified issuecontext addissue func

--- frontend/src/context/IssueContext.jsx
+++ frontend/src/context/IssueContext.jsx
@@ -1,15 +1,29 @@
 import React, { createContext, useState } from 'react';
 
+
 export const IssuesContext = createContext();
 
+
 export const IssuesProvider = ({ children }) => {
-    const [issues, setIssues] = useState([]);
+    
+    const [issues, setIssues] = useState(() => {
+        
+        const storedIssues = JSON.parse(localStorage.getItem('issues')) || [];
+        return storedIssues;
+    });
+
+   
     const [notificationMessage, setNotificationMessage] = useState(null);
 
-    const addIssue = (newissue) => {
-        setIssues((prevIssues) => [...prevIssues, newissue]);
-        localStorage.setItem('issues', JSON.stringify([...issues, newissue]));
+    
+    const addIssue = (newIssue) => {
+        setIssues((prevIssues) => {
+            const updatedIssues = [...prevIssues, newIssue];
+            localStorage.setItem('issues', JSON.stringify(updatedIssues));
+            return updatedIssues;
+        });
     };
+
 
     return (
         <IssuesContext.Provider value={{ issues, addIssue, notificationMessage, setNotificationMessage }}>
