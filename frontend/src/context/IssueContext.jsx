27c9d0771e800866a33f56d859eb97debeab682c
Alvin11David <alvin69david@gmail.com>
2025-04-23 22:36:54 +0300

    added badge count to the issuecontext

--- frontend/src/context/IssueContext.jsx
+++ frontend/src/context/IssueContext.jsx
@@ -1,32 +1,38 @@
 import React, { createContext, useState } from 'react';
 
-
 export const IssuesContext = createContext();
 
-
 export const IssuesProvider = ({ children }) => {
-    
     const [issues, setIssues] = useState(() => {
-        
         const storedIssues = JSON.parse(localStorage.getItem('issues')) || [];
         return storedIssues;
     });
 
-   
     const [notificationMessage, setNotificationMessage] = useState(null);
+    const [badgeCount, setBadgeCount] = useState(0); 
 
-    
     const addIssue = (newIssue) => {
         setIssues((prevIssues) => {
             const updatedIssues = [...prevIssues, newIssue];
             localStorage.setItem('issues', JSON.stringify(updatedIssues));
             return updatedIssues;
         });
+
+    
+        setBadgeCount((prevCount) => prevCount + 1);
     };
 
-
     return (
-        <IssuesContext.Provider value={{ issues, addIssue, notificationMessage, setNotificationMessage }}>
+        <IssuesContext.Provider
+            value={{
+                issues,
+                addIssue,
+                notificationMessage,
+                setNotificationMessage,
+                badgeCount,
+                setBadgeCount,
+            }}
+        >
             {children}
         </IssuesContext.Provider>
     );
