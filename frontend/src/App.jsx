b9a8fbda2fc8738962fbe26a7dc986fa66194b90
Alvin11David <alvin69david@gmail.com>
2025-04-24 19:55:58 +0300

    modified app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,6 +30,7 @@ import LecturerIssues from './pages/lecturerissues';
 import { IssuesProvider } from './context/IssueContext';
 import RoleBasedRoute from './components/rolebassedroute';
 
+
 const App = () => {
   return (
     <IssuesProvider>
