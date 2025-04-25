e0b38aa3d0419dff42d7e8497d65716d1e8749c2
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-04-25 10:40:54 +0300

    edited email body

--- backend/api/emailutils.py
+++ backend/api/emailutils.py
@@ -3,9 +3,16 @@ from django.conf import settings
 
 from .models import Lecturer, Issue
 
+def notification_email(issue_id, registrar_name):
+    
+    issue = Issue.objects.get(id=issue_id)
+    lecturer = issue.assigned_to  # Using the 'assigned_to' field to get the lecturer
+    if not lecturer:
+         return {"success": False, "message": "No lecturer assigned to this issue."}
 
-"""defining a function for sending emails to lecturers when an issue hs been assigned"""
-def send_email_to_lecturer(lecturer_email,title,student_name,issue_id):
+    recipient_email = lecturer.email
+    lecturer_name = f"{lecturer.first_name} {lecturer.last_name}"  # Get the lecturer's full name
+
    subject = "New Issue Assigned"
    body = f"""
    Dear Lecturer,
