2032117e5eed6a590d68ffb20cde61ef414661d7
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-04-25 10:24:19 +0300

    email body

--- backend/api/emailutils.py
+++ backend/api/emailutils.py
@@ -1,1 +1,12 @@
 """defining a function for sending emails to lecturers when an issue hs been assigned"""
+def send_email_to_lecturer(lecturer_email,student_name,issue_id):
+   subject = "New Issue Assigned"
+   body = f"""
+   Dear Lecturer,
+   You have been assigned a new issue with ID: {issue_id}.
+   Issue details:
+   student_name: {student_name}
+   Best regards,
+   Your Team"""
+
+   
