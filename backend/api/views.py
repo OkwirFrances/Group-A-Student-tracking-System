e253ee3d135048e133f7139e902fe28ea458de39
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-04-26 18:11:17 +0300

    improvements

--- backend/api/views.py
+++ backend/api/views.py
@@ -45,18 +45,7 @@ def generate_otp():
     
 
 
-#     cached_data = cache.get(f'otp_{email}')
-#     if not cached_data:
-#         return JsonResponse({'error': 'Invalid or expired OTP'}, status=status.HTTP_400_BAD_REQUEST)
-    
-#     if cached_data['otp'] == otp:
-#         # Create user after OTP verification
-#         user = User.objects.create_user(
-#             fullname=cached_data['fullname'], 
-#             email=email, 
-#             password=cached_data['password'], 
-#             role=cached_data['role']
-#         )
+
         
 #         user.is_verified = True
 #         user.save()
