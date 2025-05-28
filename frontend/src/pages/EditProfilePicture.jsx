b9fd180705eca71b855e422a1ce40344f8832d4d
AKELLO-LILIAN <akellolilian414@gmail.com>
2025-05-28 19:19:48 +0300

    removed import

--- frontend/src/pages/EditProfilePicture.jsx
+++ frontend/src/pages/EditProfilePicture.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './EditProfilePicture.css';
 
 const EditProfilePicture = () => {
     const [profilePic, setProfilePic] = useState(null);
